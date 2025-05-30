f122cbbd0333f9632da1ca5783a9457eee4a64d3
Nemanja <nemanjavasa@gmail.com>
2025-05-30 09:09:15 +0200

    chore: Add prettier config

--- fe-client/src/App.tsx
+++ fe-client/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <div className="card">
-                <p>Spring Authorization Server with Spring Cloud Gateway as BFF</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="card">
+        <p>Spring Authorization Server with Spring Cloud Gateway as BFF</p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
