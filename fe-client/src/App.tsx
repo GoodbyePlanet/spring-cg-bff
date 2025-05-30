adc9727ded29b99dd05036b7376ee8c948ddadb9
Nemanja <nemanjavasa@gmail.com>
2025-05-30 09:05:00 +0200

    feat: Init frontend client app

---
+++ fe-client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="card">
+                <p>Spring Authorization Server with Spring Cloud Gateway as BFF</p>
+            </div>
+        </>
+    )
+}
+
+export default App
