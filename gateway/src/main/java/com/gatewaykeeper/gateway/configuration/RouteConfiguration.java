0cfa98b9a1e02acc0a7b16718cda93de656d2186
Nemanja <nemanjavasa@gmail.com>
2025-05-30 09:34:04 +0200

    chore: Add editor configuration

--- gateway/src/main/java/com/gatewaykeeper/gateway/configuration/RouteConfiguration.java
+++ gateway/src/main/java/com/gatewaykeeper/gateway/configuration/RouteConfiguration.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouteConfiguration {
 
-    private final String secureResourceUrl;
-    private final String authServerUrl;
-    private final TokenRelayGatewayFilterFactory tokenRelay;
+	private final String secureResourceUrl;
+	private final String authServerUrl;
+	private final TokenRelayGatewayFilterFactory tokenRelay;
 
     public RouteConfiguration(TokenRelayGatewayFilterFactory tokenRelay,
                               @Value("${oauth2.secure-resource-url}") String secureResourceUrl,
@@ -22,16 +22,16 @@ public class RouteConfiguration {
         this.authServerUrl = authServerUrl;
     }
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("resource", r -> r.path("/resource")
-                        .filters(f -> f.filters(tokenRelay.apply())
-                                .removeRequestHeader("Cookie")) // Prevents cookie being sent downstream
-                        .uri(secureResourceUrl))
-                .route("userinfo", r -> r.path("/userinfo")
-                        .filters(f -> f.filters(tokenRelay.apply()))
-                        .uri(authServerUrl))
-                .build();
-    }
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("resource", r -> r.path("/resource")
+				.filters(f -> f.filters(tokenRelay.apply())
+					.removeRequestHeader("Cookie")) // Prevents cookie being sent downstream
+				.uri(secureResourceUrl))
+			.route("userinfo", r -> r.path("/userinfo")
+				.filters(f -> f.filters(tokenRelay.apply()))
+				.uri(authServerUrl))
+			.build();
+	}
 }
